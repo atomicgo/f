8c3edee8e1eda5a95243398998ac00059b3ec0e8
MarvinJWendt <git@marvinjwendt.com>
2023-10-30 20:55:58 +0100

    added more tests

--- f.go
+++ f.go
@@ -1,11 +1,18 @@
 package f
 
-func Format(template string, data any) string {
+func Format(template string, data ...any) string {
 	res, _ := FormatSafe(template, data)
 	return res
 }
 
-func FormatSafe(template string, data any) (string, error) {
+func FormatSafe(template string, data ...any) (string, error) {
+	var d any
+
+	if len(data) > 0 {
+		d = data[0]
+	}
+
 	parsed := Parse(template)
-	return parsed.Eval(data)
+
+	return parsed.Eval(d)
 }
--- f_test.go
+++ f_test.go
@@ -20,6 +20,9 @@ func TestFormat(t *testing.T) {
 		{"${Name} is 22 years old? ${Age == 22}", marvin, "Marvin is 22 years old? true"},
 		{"${Name} is 22 years old? ${Age == 22 ? 'yes' : 'no'}", marvin, "Marvin is 22 years old? yes"},
 		{"${Name} is 60 years old? ${Age == 60 ? 'yes' : 'no'}", marvin, "Marvin is 60 years old? no"},
+
+		{"${1+2}", nil, "3"},
+		{"${1+2*3}", nil, "7"},
 	}
 
 	for _, tt := range tests {
