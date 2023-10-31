fe5e0b690a791ba6c8991f0a912b52b9dc1403af
Marvin Wendt <git@marvinjwendt.com>
2023-10-31 10:47:33 +0100

    added example and improved godoc (#2)
    * added example and improved godoc
    
    * fixed linting

--- doc.go
+++ doc.go
@@ -1,4 +1,10 @@
 /*
 F is the closest thing to template literals in Go.
+
+F is a simple, fast and safe way to format strings in Go, with a familiar syntax.
+It evaluates expressions inside `${}` and replaces them with their values.
+
+The expressions support many operators, including ternary operator, and function calls.
+See the syntax here: https://expr.medv.io/docs/Language-Definition
 */
 package f
--- examples_test.go
+++ examples_test.go
@@ -1,1 +1,34 @@
 package f_test
+
+import (
+	"fmt"
+
+	"atomicgo.dev/f"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func Example_demo() {
+	// Format a string with a struct
+	john := Person{Name: "Bob", Age: 22}
+	fmt.Println(f.Format("${Name} is ${Age} years old", john))
+
+	// Format a string with a map
+	alice := map[string]any{"Name": "Alice", "Age": 27}
+	fmt.Println(f.Format("${Name} is ${Age} years old", alice))
+
+	// Evaluate an expression
+	fmt.Println(f.Format("John is 22 years old: ${Age == 22}", john))
+
+	// Ternary operator
+	fmt.Println(f.Format("John is 22 years old: ${Age == 22 ? 'yes' : 'no'}", john))
+
+	// Output:
+	// Bob is 22 years old
+	// Alice is 27 years old
+	// John is 22 years old: true
+	// John is 22 years old: yes
+}
